7f85a8430a42b00b4008ff1f8cad5b129a916f64
Misafir Ogrenci <ogrenci@lab.local>
2026-02-17 14:36:41 +0300

    ilk kurulum yapıldı ve proje dosyaları eklendi

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+
+      <div>
+        <h1>Web Tasarımı ve Programlama</h1>
+        <h2>LAB-1</h2>
+        <p>Ad Soyad : İshak KARATAŞ</p>
+        <p>Ogrenci No : 235541073</p>
+      </div>
+
+  )
+}
+
+export default App
